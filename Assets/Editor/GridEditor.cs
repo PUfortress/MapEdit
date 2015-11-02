e24ba369c7159f9a9ec8d9e6d60007bfeab84b00
jongwoo <kjwoo810@gmail.com>
2015-11-03 03:24:41 +0900

    finally create Map Objectgit add . hihhihhihhihhihhihhihhih

--- Assets/Editor/GridEditor.cs
+++ Assets/Editor/GridEditor.cs
@@ -114,5 +114,35 @@ public class GridEditor : Editor {
 
         return sliderPosition;
     }
+    void OnSceneGUI()
+    {
+        int controlId = GUIUtility.GetControlID(FocusType.Passive);
+        Event e = Event.current;
+        Ray ray = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
+        Vector3 mousePos = ray.origin;
 
+        if(e.isMouse && e.type == EventType.MouseDown)
+        {
+            GUIUtility.hotControl = controlId;
+            e.Use();
+
+            GameObject gameObject;
+            Transform prefab = grid.tilePrefab;
+
+            if(prefab)
+            {
+                Undo.IncrementCurrentGroup();
+                gameObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab.gameObject);
+                Vector3 aligned = new Vector3(Mathf.Floor(mousePos.x / grid.width) * grid.width + grid.width / 2.0f, Mathf.Floor(mousePos.y / grid.height) * grid.height + grid.height / 2.0f, 0.0f);
+                gameObject.transform.position = aligned;
+                gameObject.transform.parent = grid.transform;
+                Undo.RegisterCreatedObjectUndo(gameObject, "Create" + gameObject.name);
+            }
+        }
+        if (e.isMouse && e.type == EventType.MouseUp)
+        {
+            GUIUtility.hotControl = 0;
+        }
+    }
+    
 }
